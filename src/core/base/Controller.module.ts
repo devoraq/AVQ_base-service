3374cd096379b1bc6d80fae772bf7466a4d7cea6
chyVacheck <dmytro.shah.work@gmail.com>
2025-10-22 21:51:48 +0200

    feat: add ControllerModule

---
+++ src/core/base/Controller.module.ts
@@ -0,0 +1,45 @@
+/**
+ * @file Controller.module.ts
+ * @module core/base
+ *
+ * @description
+ * Базовый абстрактный класс для gRPC-контроллеров с поддержкой middleware
+ * до и после выполнения контроллера. Служит фундаментом для системных
+ * и прикладных контроллеров.
+ *
+ * @extends BaseModule
+ *
+ * @see BaseModule
+ * @see EModuleType
+ *
+ * @example
+ * class SomeCustomController extends ControllerModule {
+ *   constructor() {
+ *     super('SomeController');
+ *   }
+ * }
+ */
+
+/**
+ * ! my imports
+ */
+import { BaseModule } from '@core/base/Base.module';
+import { EModuleType, type TRpcController } from '@core/types';
+
+/**
+ * Абстрактный класс, описывающий базовые свойства всех контроллеров: тип и имя контроллера.
+ * Используется как фундамент для лог-контекста и архитектурного разграничения.
+ */
+export abstract class ControllerModule extends BaseModule {
+	/**
+	 * @param moduleName Название контроллера (для лог-контекста)
+	 */
+	protected constructor(moduleName: string) {
+		super(EModuleType.CONTROLLER, moduleName);
+	}
+
+	/**
+	 * Метод контроллера.
+	 */
+	abstract handle: TRpcController;
+}
--- src/core/base/index.ts
+++ src/core/base/index.ts
@@ -15,9 +15,5 @@ export * from './Base.module';
 // extends BaseModule
 export * from './Middleware.module';
 export * from './Router.module';
+export * from './Controller.module';
 export * from './Service.module';
-// export * from './BaseDatabase.module';
-// export * from './BaseGuard.module';
-
-// export * from './repository.module';
-// export * from './service.module';
